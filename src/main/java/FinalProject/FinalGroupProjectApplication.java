5ab1331c7e17875420a2974ab58d0452dfaf106c
Scott Grigsby <sdgrigsby@dmacc.edu>
2023-03-29 11:45:02 -0500

    initial commit

---
+++ src/main/java/FinalProject/FinalGroupProjectApplication.java
@@ -0,0 +1,13 @@
+package FinalProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalGroupProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalGroupProjectApplication.class, args);
+	}
+
+}
